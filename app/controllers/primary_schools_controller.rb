8a2d2bde96f25707d6585847d0e3843ba3b45001
Yasuda Toshiyuki <toshi1973814@gmail.com>
2017-06-18 08:51:12 +0000

    クリック地点の緯度経度をhidden_tagにセットして送信できるようにする

--- app/controllers/primary_schools_controller.rb
+++ app/controllers/primary_schools_controller.rb
@@ -3,10 +3,12 @@ class PrimarySchoolsController < ApplicationController
   def search
     if params[:lat].present? && params[:lng].present?
       @results = PrimarySchool.search( lat: params[:lat], lng: params[:lng] )
+      @place = Place.new(latitude: params[:lat], longitude: params[:lng])
+    else
+      @place = Place.first
     end
 
-    @places = Place.first
-    @hash = Gmaps4rails.build_markers(@places) do |place, marker|
+    @hash = Gmaps4rails.build_markers(@place) do |place, marker|
       marker.lat place.latitude
       marker.lng place.longitude
       marker.infowindow place.name

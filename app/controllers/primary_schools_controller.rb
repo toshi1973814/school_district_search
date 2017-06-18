14e09f7945c6b3c1f359e0138c9e7e5123995447
Yasuda Toshiyuki <toshi1973814@gmail.com>
2017-06-18 13:16:17 +0000

    Basic認証をかけられるようにする

--- app/controllers/primary_schools_controller.rb
+++ app/controllers/primary_schools_controller.rb
@@ -1,5 +1,9 @@
 class PrimarySchoolsController < ApplicationController
 
+  if ENV["PRIMARY_SCHOOL_SEARCH_AUTH_ID"] && ENV["PRIMARY_SCHOOL_SEARCH_AUTH_PW"]
+    http_basic_authenticate_with name: ENV["PRIMARY_SCHOOL_SEARCH_AUTH_ID"], password: ENV["PRIMARY_SCHOOL_SEARCH_AUTH_PW"]
+  end
+
   def search
     if params[:lat].present? && params[:lng].present?
       @results = PrimarySchool.search( lat: params[:lat], lng: params[:lng] )

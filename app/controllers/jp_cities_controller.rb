eb61da6415466b661b8b88845c49d73775552066
Yasuda Toshiyuki <toshi1973814@gmail.com>
2017-06-19 14:40:07 +0000

    構築の際に参考に作成したコントローラを削除

--- app/controllers/jp_cities_controller.rb
+++
@@ -1,9 +0,0 @@
-class JpCitiesController < ApplicationController
-
-  def search
-    if params[:lat].present? && params[:lng].present?
-      @results = JpCity.search( lat: params[:lat], lng: params[:lng] )
-    end
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
 
   # root to: 'jp_cities#search'
   root to: 'primary_schools#search'
-  get '/search' => 'search#search', as: :search
-  get '/jp_cities' => 'jp_cities#search', as: :jp_cities
   get '/primary_schools' => 'primary_schools#search', as: :primary_schools
 end
